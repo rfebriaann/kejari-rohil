1e76e8ae4e54725caed10417c9a8bf458ec8ca2a
rfebriaann <raflyfebriann@gmail.com>
2024-10-30 20:37:16 +0700

    update shadow dan component

--- resources/views/livewire/guest/homepage.blade.php
+++ resources/views/livewire/guest/homepage.blade.php
@@ -6,25 +6,31 @@
                     
                 </div>
                 <div class="flex gap-4 h-full w-full basis-1/3">
-                    <div class="bg-[#ab7743] h-full w-full rounded-3xl">
+                    <div class="bg-[#ab7743] border-2 border-[#5b3018] h-full w-full rounded-3xl" style="box-shadow: 0px 10px 0 rgba(87, 46, 23, 1);">
                         <div class="flex h-full justify-center items-center gap-5 mx-10">
                             <div class="basis-1/3">
                                 <img class="size-40" src="{{ asset('storage/assets/img/illustrasi/illu1.svg') }}" class="h-12 mr-3" alt="Logo" />
                             </div>
                             <div class="basis-2/3">
-                                <h2 class="font-montserrat font-bold text-2xl">Form pengambilan barang bukti</h2>
-                                <p class="font-montserrat font-medium text-lg leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum delectus, animi possimus maiores mollitia.</p>
+                                <div class="flex flex-col gap-3">
+                                    <h2 class="font-montserrat font-bold text-2xl">Form pengambilan barang bukti</h2>
+                                    <p class="font-montserrat font-medium text-lg leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum delectus, animi possimus maiores mollitia.</p>
+                                    <a href="" class="font-montserrat font-semibold px-4 py-1.5 rounded-3xl bg-white w-1/2 text-center" style="box-shadow: 0px 5px 0 rgb(211, 211, 211);">Mulai input form</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="bg-[#d7bda6] w-full h-full rounded-3xl">
+                    <div class="bg-[#d7bda6] border-2 border-[#5b3018] w-full h-full rounded-3xl" style="box-shadow: 0px 10px 0 rgba(87, 46, 23, 1);">
                         <div class="flex h-full justify-center items-center gap-5 mx-10">
                             <div class="basis-1/3">
                                 <img class="size-40" src="{{ asset('storage/assets/img/illustrasi/illu2.svg') }}" class="h-12 mr-3" alt="Logo" />
                             </div>
                             <div class="basis-2/3">
-                                <h2 class="font-montserrat font-bold text-2xl">Form pengambilan barang bukti</h2>
-                                <p class="font-montserrat font-medium text-lg leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum delectus, animi possimus maiores mollitia.</p>
+                                <div class="flex flex-col gap-3">
+                                    <h2 class="font-montserrat font-bold text-2xl">Data Perkara</h2>
+                                    <p class="font-montserrat font-medium text-lg leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum delectus, animi possimus maiores mollitia.</p>
+                                    <a href="" class="font-montserrat font-semibold px-4 py-1.5 rounded-3xl bg-white w-1/2 text-center" style="box-shadow: 0px 5px 0 rgb(211, 211, 211);">Lihat data perkara</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -42,60 +48,53 @@
                 <ol class="relative ms-3 border-s-4 border-[#D7BDA6]">
                     <li class="mb-10 ms-6">
                     <span class="absolute -start-[18px] flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white dark:bg-[#AB7743] dark:ring-[#5B3018]">
-                        <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        {{-- <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                        </svg>
+                        </svg> --}}
+                        <h1 class="text-white font-montserrat font-bold">1</h1>
                     </span>
                     <div class=" ml-4">
-                        <h4 class="p-2 text-base font-semibold text-gray-900">Estimated delivery in 24 Nov 2023</h4>
+                        <h4 class="p-2 text-base font-semibold text-gray-900">Langkah ke 1</h4>
                         {{-- <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Products delivered</p> --}}
                     </div>
                     </li>
 
                     <li class="mb-10 ms-6">
                         <span class="absolute -start-[18px] flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white dark:bg-[#AB7743] dark:ring-[#5B3018]">
-                            <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                            </svg>
+                            <h1 class="text-white font-montserrat font-bold">2</h1>
                         </span>
                         <div class=" ml-4">
-                            <h4 class="p-2 text-base font-semibold text-gray-900">Estimated delivery in 24 Nov 2023</h4>
+                            <h4 class="p-2 text-base font-semibold text-gray-900">Langkah ke 2</h4>
                             {{-- <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Products delivered</p> --}}
                         </div>
                     </li>
 
                             <li class="mb-10 ms-6">
                         <span class="absolute -start-[18px] flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white dark:bg-[#AB7743] dark:ring-[#5B3018]">
-                            <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                            </svg>
+                            <h1 class="text-white font-montserrat font-bold">3</h1>
                         </span>
                         <div class=" ml-4">
-                            <h4 class="p-2 text-base font-semibold text-gray-900">Estimated delivery in 24 Nov 2023</h4>
+                            <h4 class="p-2 text-base font-semibold text-gray-900">Langkah ke 3</h4>
                             {{-- <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Products delivered</p> --}}
                         </div>
                     </li>
 
                     <li class="mb-10 ms-6">
                         <span class="absolute -start-[18px] flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white dark:bg-[#AB7743] dark:ring-[#5B3018]">
-                            <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                            </svg>
+                            <h1 class="text-white font-montserrat font-bold">4</h1>
                         </span>
                         <div class=" ml-4">
-                            <h4 class="p-2 text-base font-semibold text-gray-900">Estimated delivery in 24 Nov 2023</h4>
+                            <h4 class="p-2 text-base font-semibold text-gray-900">Langkah ke 4</h4>
                             {{-- <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Products delivered</p> --}}
                         </div>
                     </li>
 
                     <li class="ms-6">
                         <span class="absolute -start-[18px] flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white dark:bg-[#AB7743] dark:ring-[#5B3018]">
-                            <svg class="h-6 w-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                            </svg>
+                            <h1 class="text-white font-montserrat font-bold">5</h1>
                         </span>
                         <div class=" ml-4">
-                            <h4 class="p-2 text-base font-semibold text-gray-900">Estimated delivery in 24 Nov 2023</h4>
+                            <h4 class="p-2 text-base font-semibold text-gray-900">Langkah ke 5</h4>
                             {{-- <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Products delivered</p> --}}
                         </div>
                     </li>
